072718cd6c239591eb79fab916ffe72954b7adce
Godskitchen <diemygod@gmail.com>
2023-05-08 01:13:59 +0300

    stream fix

--- src/core/cli-command/generate.command.ts
+++ src/core/cli-command/generate.command.ts
@@ -3,23 +3,19 @@ import got from 'got';
 import { CliCommandInterface } from './cli-command.interface.js';
 import OfferGenerator from '../../modules/offer-generator/offer-generator.js';
 import TSVFileWriter from '../file-writer/ftsv-file-writer.js';
-
 import type { MockData } from '../../types/mock-data.type.js';
 
 export default class GenerateCommand implements CliCommandInterface {
   public readonly name = '--generate';
   private initialData!: MockData;
 
-  public async execute(...params:string[]): Promise<void> {
+  public async execute(...params: string[]): Promise<void> {
     const [count, filepath, url] = params;
     const offerCount = Number.parseInt(count, 10);
 
-    try {
-      this.initialData = await got.get(url).json();
-    } catch {
-      console.log(`Can't fetch data from ${url}.`);
-      return;
-    }
+    this.initialData = await got.get(url).json().catch(() => {
+      throw new Error(url ? `Can't fetch data from ${url}.` : 'incorrect filepath');
+    }) as MockData;
 
     const offerGenerator = new OfferGenerator(this.initialData);
     const tsvFileWriter = new TSVFileWriter(filepath);
